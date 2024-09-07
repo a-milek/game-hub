4a3d2cb82090652b5ae9c73de8b8271706ecdcaa
a-milek <anna@milek.eu.org>
2024-09-07 13:42:41 +0200

    Dark Mode Implementation

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,6 +1,7 @@
 import { HStack, Image, Text } from "@chakra-ui/react";
 import logo from "../assets/logo.png";
 import { Input } from "@chakra-ui/react";
+import { Switch } from "@chakra-ui/react";
 
 const NavBar = () => {
   return (
@@ -8,6 +9,7 @@ const NavBar = () => {
       <Image src={logo} boxSize="60px"></Image>
 
       <Input placeholder="Search" />
+      <Switch colorScheme="teal" size="lg" />
     </HStack>
   );
 };
