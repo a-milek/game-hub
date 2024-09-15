c58012b6a190dc659869f8c2985d321a06fb6d4c
a-milek <anna@milek.eu.org>
2024-09-15 13:22:26 +0200

    Platform Selector

---
+++ src/components/PlatformSelector.tsx
@@ -0,0 +1,25 @@
+import { Menu, MenuButton, Button, MenuList, MenuItem } from "@chakra-ui/react";
+import { FaChevronDown } from "react-icons/fa";
+import usePlatforms from "../hooks/usePlatforms";
+
+const PlatformSelector = () => {
+  const { data, error } = usePlatforms();
+
+  if (error) return null;
+  return (
+    <>
+      <Menu>
+        <MenuButton as={Button} rightIcon={<FaChevronDown />}>
+          Platforms
+        </MenuButton>
+        <MenuList>
+          {data.map((platform) => (
+            <MenuItem key={platform.id}>{platform.name}</MenuItem>
+          ))}
+        </MenuList>
+      </Menu>
+    </>
+  );
+};
+
+export default PlatformSelector;
